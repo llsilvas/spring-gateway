79155d5914b4fb6d66f88a284b2c1218b0fd3234
Leandro <leandroasilva@apoioprodesp.sp.gov.br>
2022-04-02 23:28:48 -0300

    Initial commit

---
+++ src/main/java/com/example/gateway/FallbackController.java
@@ -0,0 +1,13 @@
+package com.example.gateway;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallbackController {
+
+    @GetMapping("/fallback1")
+    public String userFallback(){
+        return "User service is not available";
+    }
+}
---
+++ src/main/java/com/example/gateway/config/GatewayConfig.java
@@ -0,0 +1,28 @@
+package com.example.gateway.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@EnableHystrix
+@Configuration
+public class GatewayConfig {
+
+    @Autowired
+    AuthenticationFilter filter;
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route("user-service", r -> r.path("/users/**")
+                        .filters(f -> f.filter(filter))
+                        .uri("http://localhost:8081/"))
+                .route("auth-service", r -> r.path("/auth/**")
+                        .filters(f -> f.filter(filter))
+                        .uri("http://localhost:8087/"))
+                .build();
+    }
+}
